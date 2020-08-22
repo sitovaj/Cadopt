a52b627ee284a343e61fd12d402afefec620abaf
sitovaj <sitovaj@gmail.com>
2020-08-22 17:09:47 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,231 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+from models import setup_db, Cat, Curator
+from auth import AuthError, requires_auth
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    @app.route("/")
+    def hello():
+        return "Hello World!"
+
+    @app.route("/cats")
+    def get_cat_list():
+        selection = Cat.query.order_by('id').all()
+        if len(selection) == 0:
+            return jsonify({
+                'success': False,
+                'message': 'No cats for now'
+            })
+        formatted_cats = [cat.serialize_cat() for cat in selection]
+
+        return jsonify({
+            'success': True,
+            'cats': formatted_cats
+        }), 200
+
+    @app.route("/cats/<id>")
+    def get_cat_id(id):
+        body = request.get_json()  # get request information sent within URL
+        selection = Cat.query.filter(Cat.id == id).one_or_none()
+        if selection is None:
+            return jsonify({
+                'success': False,
+                'message': 'Id not found'
+            }), 400
+        return jsonify({
+            'success': True,
+            'cats': selection.serialize_cat()
+        }), 200
+
+    @app.route("/cats", methods=['POST'])
+    @requires_auth('post:cats')
+    def new_cat(payload):
+        body = request.get_json()
+        if body is None:
+            abort(404)
+        new_name = body.get('name', None)
+        new_gender = body.get('gender', None)
+        new_birthday = body.get('birthday', None)
+        new_color = body.get('color', None)
+        new_city = body.get('city', None)
+        new_image = body.get('image', None)
+        new_status = body.get('status', None)
+        new_curator = body.get('curator_id', None)
+
+        try:
+            cat = Cat(name=new_name, gender=new_gender, birthday=new_birthday,
+                      color=new_color, city=new_city, image=new_image, status=new_status, curator_id=new_curator)
+            cat.insert()
+            return jsonify({
+                'success': True,
+                'cat': cat.serialize_cat()
+            })
+        except Exception:
+            abort(422)
+
+    @app.route('/cats/<id>', methods=['PATCH'])
+    @requires_auth('patch:cats')
+    def patch_cats(payload, id):
+        body = request.get_json()  # get request information sent within URL
+        try:
+            cat = Cat.query.filter(Cat.id == id).one_or_none()
+            if not cat:
+                abort(404)
+            if 'status' in body:
+                cat.status = body.get('status')
+            if 'gender' in body:
+                cat.gender = body.get('gender')            
+            if 'city' in body:
+                cat.city = body.get('city')
+            cat.update()
+
+            return jsonify({
+                'success': True,
+                'cat': cat.serialize_cat()
+            })
+        except Exception:
+            abort(400)
+
+
+    @app.route('/cats/<id>', methods=['DELETE'])
+    @requires_auth('delete:cats')
+    def delete_cats(payload, id):
+        try:
+            cat = Cat.query.filter(Cat.id == id).one_or_none()
+            if not cat:
+                abort(404)
+            cat.delete()
+
+            return jsonify({
+                'success': True,
+                'delete': id
+            })
+        except Exception:
+            abort(400)
+
+    @app.route("/curators")
+    def get_curator_list():
+        selection = Curator.query.order_by('id').all()
+        if len(selection) == 0:
+            return jsonify({
+                'success': False,
+                'message': 'No curator for now'
+            })
+        formatted_curators = [curator.serialize_curator()
+                              for curator in selection]
+
+        return jsonify({
+            'success': True,
+            'curators': formatted_curators
+        }), 200
+
+    @app.route("/curators/<id>")
+    def get_curator_id(id):
+        body = request.get_json()
+        selection = Curator.query.filter(Curator.id == id).one_or_none()
+        if selection is None:
+            return jsonify({
+                'success': False,
+                'message': 'Id not found'
+            })
+        return jsonify({
+            'success': True,
+            'curators': selection.serialize_curator()
+        }), 200
+
+    @app.route('/curators', methods=['POST'])
+    @requires_auth('post:curators')
+    def new_curator(payload):
+        body = request.get_json()
+        if body is None:
+            abort(404)
+        new_name = body.get('name', None)
+        new_legal_number = body.get('legal_number', None)
+        new_phone = body.get('phone', None)
+        new_email = body.get('email', None)
+        new_facebook = body.get('facebook', None)
+        try:
+            curator = Curator(name=new_name, legal_number=new_legal_number,
+                            phone=new_phone, email=new_email, facebook=new_facebook, cats=[])
+            curator.insert()
+            return jsonify({
+                'success': True,
+                'curators': curator.serialize_curator()
+            })
+        except Exception:
+            abort(422)
+
+    @app.route('/curators/<id>', methods=['DELETE'])
+    @requires_auth('delete:curators')
+    def delete_curators(payload, id):
+        try:
+            selection = Curator.query.filter(Curator.id == id).one_or_none()
+            if not selection:
+                abort(404)
+            selection.delete()
+
+            return jsonify({
+                'success': True,
+                'delete': id
+            })
+        except Exception:
+            abort(400)
+
+# Error handlers
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "Not found"
+        }), 404
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "Bad request, server couldnt understand it"
+        }), 400
+
+    @app.errorhandler(422)
+    def unprocessable_entity(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "Unprocessable Entity, check your request"
+        }), 422
+
+    @app.errorhandler(405)
+    def not_allowed(error):
+        return jsonify({
+            "success": False,
+            "error": 405,
+            "message": "Method not allowed"
+        }), 405
+
+    @app.errorhandler(401)
+    def unauthorized(error):
+        return jsonify({
+            "success": False,
+            "error": 401,
+            "message": "Unauthorized request"
+        }), 401
+
+    return app
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
---
+++ models.py
@@ -0,0 +1,181 @@
+
+import os
+from sqlalchemy import Column, String, Integer, DateTime, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_name = "cadopt"
+database_path = "postgres://{}:{}@{}/{}".format(
+    'student', 'student', 'localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.drop_all()
+    db.create_all()
+
+
+'''
+Cat
+
+'''
+
+
+class Cat(db.Model):
+    __tablename__ = 'cats'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    gender = Column(String)
+    birthday = Column(DateTime)
+    color = Column(String)
+    city = Column(String)
+    image = Column(String)
+    status = Column(String)
+    # curator_id = Column(Integer, db.ForeignKey('curators.id'), primary_key=False)
+    # curator = db.relationship("Curator", back_populates="cats")
+    curator_id = db.Column(db.Integer, db.ForeignKey('curators.id'),nullable=False)
+
+    def __init__(self, name, gender, birthday, color, city, image, status, curator_id):
+    
+        self.name = name
+        self.gender = gender
+        self.birthday = birthday        
+        self.color = color
+        self.city = city
+        self.image = image
+        self.status = status
+        self.curator_id = curator_id
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'gender': self.gender,
+            'birthday': self.birthday,
+            'color': self.color,
+            'city': self.city,
+            'image': self.image,
+            'status': self.status,
+            'curator_id': self.curator_id
+        }
+
+# Upload serializer
+    def serialize_cat_bis(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'gender': self.gender,
+            'birthday': self.birthday,
+            'color': self.color,
+            'city': self.city,
+            'image': self.image,
+            'status': self.status
+        }
+
+    def serialize_cat(self):
+        if self.curator:
+            dict_curator = self.curator.serialize_curator_bis()
+        return {
+            'id': self.id,
+            'name': self.name,
+            'gender': self.gender,
+            'birthday': self.birthday,
+            'color': self.color,
+            'city': self.city,
+            'image': self.image,
+            'status': self.status,
+            'curator': dict_curator
+        }
+
+
+'''
+Curator
+
+'''
+
+
+class Curator(db.Model):
+    __tablename__ = 'curators'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    legal_number = Column(String)
+    phone = Column(String)
+    email = Column(String)
+    facebook = Column(String)
+    # cats = db.relationship("Cat", cascade="all, delete-orphan", back_populates="curator", lazy=True)
+    cats = db.relationship('Cat', backref='curator', lazy=True)
+
+    def __init__(self, name, legal_number, phone, email, facebook, cats
+    ):
+        self.name = name
+        self.legal_number = legal_number
+        self.phone = phone
+        self.email = email
+        self.facebook = facebook
+        self.cats = cats
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'legal_number': self.legal_number,
+            'phone': self.phone,
+            'email': self.email,
+            'facebook': self.facebook,
+            'cats': self.cats
+        }
+
+# curator serializer
+    def serialize_curator(self):
+        if self.cats:
+            cats = [cat.serialize_cat_bis() for cat in self.cats]
+        return {
+            'name': self.name,
+            'legal_number': self.legal_number,
+            'phone': self.phone,
+            'email': self.email,
+            'facebook': self.facebook,
+            'cats': [cat.serialize_cat_bis() for cat in self.cats]
+        }    
+    def serialize_curator_bis(self):
+        return {
+            'name': self.name,
+            'legal_number': self.legal_number,
+            'phone': self.phone,
+            'email': self.email,
+            'facebook': self.facebook
+        }
