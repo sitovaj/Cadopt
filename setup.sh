c4cefc33b2c6a6afced949e0ea7ec443fae2e0b6
sitovaj <sitovaj@gmail.com>
2020-08-22 19:37:25 +0300

    Update 4

---
+++ setup.sh
@@ -0,0 +1,7 @@
+export AUTH0_DOMAIN=dev-p8l4wm2k.eu.auth0.com
+export ALGORITHMS=RS256
+export API_AUDIENCE=CadoptAPI
+export DATABASE_URL=postgres://ifehwiaolmadrn:139c726550fa602ef4fd689bcfb93a83d07d468a660fbd3a6778dc58d32f6ee8@ec2-34-238-26-109.compute-1.amazonaws.com:5432/dc450favsn3hok
+export FLASK_APP=app.py
+export FLASK_ENV=development
+export SQLALCHEMY_DATABASE_URI=postgres://postgres@localhost:5432/
